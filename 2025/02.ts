0c47e1a5b8b4878dc6a6e5b293acf95d46c9f87c
AJ Coppa <aj.coppa@gmail.com>
2025-12-10 21:08:53 -0500

    2**

--- 2025/02.ts
+++ 2025/02.ts
@@ -1,20 +1,30 @@
+import { chunk, sum } from './lib';
+
 async function main() {
   const input = await Bun.file('02-input.txt').text();
   const ranges = parseRanges(input);
   console.log(partOne(ranges));
+  console.log(partTwo(ranges));
 }
 
 function partOne(ranges: Range[]): number {
-  const invalidIds: number[] = ranges.reduce((acc: number[], range: Range) => {
+  return sum(findInvalidIds(ranges, isInvalidId));
+}
+
+function partTwo(ranges: Range[]): number {
+  return sum(findInvalidIds(ranges, isInvalidIdTwo));
+}
+
+function findInvalidIds(ranges: Range[], predicate: (id: number) => boolean): number[] {
+  return ranges.reduce((acc: number[], range: Range) => {
     const invalidInRange: number[] = [];
     for (let i = range.start; i <= range.end; i++) {
-      if (isInvalidId(i)) {
+      if (predicate(i)) {
         invalidInRange.push(i);
       }
     }
     return [...acc, ...invalidInRange];
   }, []);
-  return invalidIds.reduce((a, b) => a + b, 0);
 }
 
 function isInvalidId(id: number): boolean {
@@ -22,6 +32,20 @@ function isInvalidId(id: number): boolean {
   const firstHalf = stringId.slice(0, stringId.length / 2);
   const secondHalf = stringId.slice(stringId.length / 2);
   return firstHalf === secondHalf;
+}
+
+function isInvalidIdTwo(id: number): boolean {
+  const stringId = id.toString();
+  for (let n = Math.floor(stringId.length / 2); n >= 1; n--) {
+    if (stringId.length % n !== 0) {
+      continue;
+    }
+    const chunks = chunk(stringId.split(''), n);
+    if (chunks.every(chunk => chunk.join('') === chunks[0].join(''))) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function parseRanges(input: string): Range[] {
--- 2025/lib.ts
+++ 2025/lib.ts
@@ -5,3 +5,16 @@ export function assert(condition: boolean, message: string) {
     console.log(`√ - ${message}`);
   }
 }
+
+export function chunk<A>(list: A[], chunkSize: number): A[][] {
+  const chunks: A[][] = [];
+  for (let i = 0; i < list.length; i += chunkSize) {
+    const chunk = list.slice(i, i + chunkSize);
+    chunks.push(chunk);
+  }
+  return chunks;
+}
+
+export function sum(list: number[]): number {
+  return list.reduce((a, b) => a + b, 0);
+}
