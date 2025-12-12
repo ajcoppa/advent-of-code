5f8ae43e0b531aee747026ee0f5e339089ed7a51
AJ Coppa <aj.coppa@gmail.com>
2025-12-11 20:52:46 -0500

    4**

--- 2025/04.ts
+++ 2025/04.ts
@@ -4,6 +4,7 @@ async function main() {
   const input = await Bun.file('04-input.txt').text();
   const grid = parseGrid(input);
   console.log(partOne(grid));
+  console.log(partTwo(grid));
 }
 
 function partOne(grid: Grid<Tile>): number {
@@ -12,6 +13,29 @@ function partOne(grid: Grid<Tile>): number {
       tile === Tile.PaperRoll && coordIsAccessible(grid, {x, y})
     )
   ).flat().length;
+}
+
+function partTwo(grid: Grid<Tile>): number {
+  let removed = 0;
+  while (partOne(grid) > 0) {
+    removed += tick(grid);
+  }
+  return removed;
+}
+
+function tick(grid: Grid<Tile>): number {
+  let coordsToRemove: Coord[] = [];
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (coordIsAccessible(grid, {x, y}) && grid[y][x] === Tile.PaperRoll) {
+        coordsToRemove.push({x, y});
+      }
+    }
+  }
+  coordsToRemove.forEach(({x, y}) => {
+    grid[y][x] = Tile.Empty;
+  });
+  return coordsToRemove.length;
 }
 
 function coordIsAccessible(grid: Grid<Tile>, coord: Coord): boolean {
