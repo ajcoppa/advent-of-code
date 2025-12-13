1e60068968688d93c65500f33e4f39e46fdfe86b
AJ Coppa <aj.coppa@gmail.com>
2025-12-12 22:27:17 -0500

    6**

--- 2025/06.ts
+++ 2025/06.ts
@@ -3,24 +3,45 @@ import { transpose } from "../lib/Coord";
 
 async function main() {
   const input = await Bun.file('06-input.txt').text();
-  const {operands, operators} = parseProblems(input);
-  console.log(partOne(operands, operators));
+  const parseOne = parseProblemsOne(input);
+  console.log(process(parseOne.operands, parseOne.operators));
+  const parseTwo = parseProblemsTwo(input);
+  console.log(process(parseTwo.operands, parseTwo.operators));
 }
 
-function partOne(operands: number[][], operators: string[]): number {
-  const transposedOperands = transpose(operands);
+function process(operands: number[][], operators: string[]): number {
   return sum(operators.map((operator, i) => {
     const combiner = operator === "+" ? sum : product;
-    return combiner(transposedOperands[i]);
+    return combiner(operands[i]);
   }));
 }
 
-function parseProblems(input: string) {
+function parseProblemsOne(input: string) {
   const lines = input.split("\n");
-  const operands = lines.slice(0, lines.length - 1).map(line => 
+  const operands = transpose(lines.slice(0, lines.length - 1).map(line => 
     line.split(" ").filter(str => !!str).map(Number)
-  );
+  ));
   const operators = lines[lines.length - 1].split(" ").filter(str => !!str);
+  return {operands, operators};
+}
+
+function parseProblemsTwo(input: string) {
+  const lines = input.split("\n");
+  const operandLines = lines.slice(0, lines.length - 1);
+  const baseOperands = operandLines.map(line => 
+    line.split("").filter(str => !!str)
+  ).reverse();
+  const operands = transpose(baseOperands).reverse().map(x =>
+    x.reverse().join("")
+  ).reduce((acc: number[][], cur: string) => 
+    cur.trim().length === 0 ?
+      // String of all spaces -> start a new grouping
+      acc.concat([[]]) :
+      // Otherwise, add the parsed number version to the last grouping
+      [...acc.slice(0, acc.length - 1), acc[acc.length - 1].concat([Number(cur)])]
+  , [[]]);
+
+  const operators = lines[lines.length - 1].split(" ").filter(str => !!str).reverse();
   return {operands, operators};
 }
 
